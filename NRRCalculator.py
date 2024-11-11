06e5734a1f7b91bdccd432b431aae2a1ba76e4ad
Sandun Akalanka <ggsakalanka@gmail.com>
2024-11-11 19:22:52 +0530

    Modified and cleaned code.

--- NRRCalculator.py
+++ NRRCalculator.py
@@ -1,16 +1,19 @@
-from decimal import Decimal, ROUND_UP
+from decimal import Decimal, getcontext
 
 print("This one is for Limited Over matches.")
-print("Even if a team gets all out before all allocated overs, their over quota will be considered as completed.")
+print("If a team gets all out before all allocated overs, their over quota will be considered as completed.")
 
-runs_for = input("Total runs by team: ")
+runs_for = input("Total runs scored: ")
 overs_for = input("Total allocated overs quota to face: ")
 runs_against = input("Total runs conceded: ")
 overs_against = input("Total allocated overs quota to bowl: ")
 
-nrr_for = float(runs_for) / float(overs_for)
-nrr_against = float(runs_against) / float(overs_against)
-nrr = nrr_for - nrr_against 
-nrr_rounded = Decimal(nrr).quantize(Decimal('.001'),rounding = ROUND_UP)
+getcontext().prec = 4
 
-print("Final NRR: " , nrr_rounded)
+nrr_for = Decimal(runs_for) / Decimal(overs_for)
+nrr_against = Decimal(runs_against) / Decimal(overs_against)
+nrr = nrr_for - nrr_against 
+
+print("NRR For: " , nrr_for)
+print("NRR Against: " , nrr_against)
+print("Final NRR: " , nrr)
